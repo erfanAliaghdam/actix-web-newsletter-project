4eb4c35cb824c2293d9cd2606c0dcf6c1a2274ea
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2024-03-19 21:14:53 +0330

    feat: add health check webservice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, http};
+
+async fn health_check(req: HttpRequest)-> impl Responder{
+    let name = req.match_info().get("name").unwrap_or("World!");
+    println!("Hello, {}!.", &name);
+    let response = serde_json::json!(
+        {
+            "status": "success",
+            "message": "server is working.... ."
+        }
+    );
+    HttpResponse::build(http::StatusCode::OK).json(response)
+}
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    println!("⚡ Server running at http://127.0.0.1:8000");
+
+    HttpServer::new(|| { App::new()
+        .route("/health-check", web::get().to(health_check))
+        .route("/health-check/{name}", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
